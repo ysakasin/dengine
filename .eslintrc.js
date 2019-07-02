2bbf69a4407e3343baf78d7df803d676965453c4
SAKATA Sinji <sinji@sakasin.net>
2019-07-02 22:00:33 +0900

    ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "env": {
+    "node": true,
+    "jest": true
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "sourceType": "module",
+    "project": "./tsconfig.json"
+  },
+  "rules": {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
+}
