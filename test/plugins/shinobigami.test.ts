65ead6c8cf0d89364eafa926c8637add5bfc1c09
SAKATA Sinji <sinji@sakasin.net>
2019-08-02 23:13:34 +0900

    Fix testcase of Shinobigami 1D6 Tables

--- test/plugins/shinobigami.test.ts
+++ test/plugins/shinobigami.test.ts
@@ -308,54 +308,94 @@ test("FT", () => {
   let randMock = new RandomMock("4/6");
   dengine.setRand(randMock);
   const input = "FT";
-  const expected =
-    "ファンブル表 ＞ 4[4] ＞ 油断した！　術の制御に失敗し、好きな【生命力】を１点失う。";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 4,
+    mainMassage: "油断した！　術の制御に失敗し、好きな【生命力】を１点失う。",
+    status: Status.Unknown,
+    process: [
+      "ファンブル表",
+      "4[4]",
+      "油断した！　術の制御に失敗し、好きな【生命力】を１点失う。"
+    ],
+    isSecret: false,
+    dice: [{ faces: 6, value: 4 }]
+  });
 });
 
 test("ET", () => {
   let randMock = new RandomMock("1/6");
   dengine.setRand(randMock);
   const input = "ET";
-  const expected = "感情表 ＞ 1[1] ＞ 共感（プラス）／不信（マイナス）";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 1,
+    mainMassage: "共感（プラス）／不信（マイナス）",
+    status: Status.Unknown,
+    process: ["感情表", "1[1]", "共感（プラス）／不信（マイナス）"],
+    isSecret: false,
+    dice: [{ faces: 6, value: 1 }]
+  });
 });
 
 test("WT", () => {
   let randMock = new RandomMock("2/6");
   dengine.setRand(randMock);
   const input = "WT";
-  const expected =
-    "変調表 ＞ 2[2] ＞ マヒ:修得済み特技がランダムに１つ使用不能になる。１サイクルの終了時に、《身体操術》で成功するとこの効果は無効化される。";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 2,
+    mainMassage:
+      "マヒ:修得済み特技がランダムに１つ使用不能になる。１サイクルの終了時に、《身体操術》で成功するとこの効果は無効化される。",
+    status: Status.Unknown,
+    process: [
+      "変調表",
+      "2[2]",
+      "マヒ:修得済み特技がランダムに１つ使用不能になる。１サイクルの終了時に、《身体操術》で成功するとこの効果は無効化される。"
+    ],
+    isSecret: false,
+    dice: [{ faces: 6, value: 2 }]
+  });
 });
 
 test("GWT", () => {
   let randMock = new RandomMock("1/6");
   dengine.setRand(randMock);
   const input = "GWT";
-  const expected =
-    "戦国変調表 ＞ 1[1] ＞ 催眠:戦闘に参加した時、戦闘開始時、もしくはこの変調を受けた時に【生命力】を1点減少しないと、戦闘から脱落する。サイクル終了時に〈意気〉判定し成功すると無効化。";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 1,
+    mainMassage:
+      "催眠:戦闘に参加した時、戦闘開始時、もしくはこの変調を受けた時に【生命力】を1点減少しないと、戦闘から脱落する。サイクル終了時に〈意気〉判定し成功すると無効化。",
+    status: Status.Unknown,
+    process: [
+      "戦国変調表",
+      "1[1]",
+      "催眠:戦闘に参加した時、戦闘開始時、もしくはこの変調を受けた時に【生命力】を1点減少しないと、戦闘から脱落する。サイクル終了時に〈意気〉判定し成功すると無効化。"
+    ],
+    isSecret: false,
+    dice: [{ faces: 6, value: 1 }]
+  });
 });
 
 test("BT", () => {
   let randMock = new RandomMock("6/6");
   dengine.setRand(randMock);
   const input = "BT";
-  const expected =
-    "戦場表 ＞ 6[6] ＞ 極地:宇宙や深海、溶岩、魔界など。ラウンドの終わりにＧＭが1D6を振り、経過ラウンド以下なら全員1点ダメージ。ここから脱落したものは変調表を適用する。";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 6,
+    mainMassage:
+      "極地:宇宙や深海、溶岩、魔界など。ラウンドの終わりにＧＭが1D6を振り、経過ラウンド以下なら全員1点ダメージ。ここから脱落したものは変調表を適用する。",
+    status: Status.Unknown,
+    process: [
+      "戦場表",
+      "6[6]",
+      "極地:宇宙や深海、溶岩、魔界など。ラウンドの終わりにＧＭが1D6を振り、経過ラウンド以下なら全員1点ダメージ。ここから脱落したものは変調表を適用する。"
+    ],
+    isSecret: false,
+    dice: [{ faces: 6, value: 6 }]
+  });
 });
 
 test("RTT", () => {
@@ -383,11 +423,20 @@ test("PLST", () => {
   let randMock = new RandomMock("2/6");
   dengine.setRand(randMock);
   const input = "PLST";
-  const expected =
-    "培養プラントシーン表 ＞ 2[2] ＞ 巨大なガラス管の中に冒涜的な生物が蠢く実験室。《意気》で判定を行い、失敗すると《マヒ》の変調を受ける。";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 2,
+    mainMassage:
+      "巨大なガラス管の中に冒涜的な生物が蠢く実験室。《意気》で判定を行い、失敗すると《マヒ》の変調を受ける。",
+    status: Status.Unknown,
+    process: [
+      "培養プラントシーン表",
+      "2[2]",
+      "巨大なガラス管の中に冒涜的な生物が蠢く実験室。《意気》で判定を行い、失敗すると《マヒ》の変調を受ける。"
+    ],
+    isSecret: false,
+    dice: [{ faces: 6, value: 2 }]
+  });
 });
 
 test("HC", () => {
@@ -494,11 +543,20 @@ test("HS", () => {
   let randMock = new RandomMock("6/6");
   dengine.setRand(randMock);
   const input = "HS";
-  const expected =
-    "催眠シーン表 ＞ 6[6] ＞ あなたは膨大な数の書架が林立する無人の図書館を歩いている。何気なく一冊の本を棚から抜き出すと、その本の向こう側にナビキャラクターの顔がのぞいている。「お前の求めるものは、その本の14ページに書かれている。」その言葉に従い、恐る恐る14ページを開いてみると……。";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 6,
+    mainMassage:
+      "あなたは膨大な数の書架が林立する無人の図書館を歩いている。何気なく一冊の本を棚から抜き出すと、その本の向こう側にナビキャラクターの顔がのぞいている。「お前の求めるものは、その本の14ページに書かれている。」その言葉に従い、恐る恐る14ページを開いてみると……。",
+    status: Status.Unknown,
+    process: [
+      "催眠シーン表",
+      "6[6]",
+      "あなたは膨大な数の書架が林立する無人の図書館を歩いている。何気なく一冊の本を棚から抜き出すと、その本の向こう側にナビキャラクターの顔がのぞいている。「お前の求めるものは、その本の14ページに書かれている。」その言葉に従い、恐る恐る14ページを開いてみると……。"
+    ],
+    isSecret: false,
+    dice: [{ faces: 6, value: 6 }]
+  });
 });
 
 test("HT", () => {
@@ -565,11 +623,20 @@ test("CLST", () => {
   let randMock = new RandomMock("6/6");
   dengine.setRand(randMock);
   const input = "CLST";
-  const expected =
-    "災厄シーン表 ＞ 6[6] ＞ 力なき者が生き残ることは出来ない。ボスが新たな力を手に入れようとしている。シーンプレイヤーが《怪力》の判定に失敗した場合、ボスに対してエニグマ：『八面六臂』が公開状態で追加される。";
   const res = dengine.roll(input);
-  expect(getFullText(res)).toBe(expected);
-  expect(res!.isSecret).toBe(false);
+  expect(res).toStrictEqual({
+    total: 6,
+    mainMassage:
+      "力なき者が生き残ることは出来ない。ボスが新たな力を手に入れようとしている。シーンプレイヤーが《怪力》の判定に失敗した場合、ボスに対してエニグマ：『八面六臂』が公開状態で追加される。",
+    status: Status.Unknown,
+    process: [
+      "災厄シーン表",
+      "6[6]",
+      "力なき者が生き残ることは出来ない。ボスが新たな力を手に入れようとしている。シーンプレイヤーが《怪力》の判定に失敗した場合、ボスに対してエニグマ：『八面六臂』が公開状態で追加される。"
+    ],
+    isSecret: false,
+    dice: [{ faces: 6, value: 6 }]
+  });
 });
 
 test("DXST", () => {
